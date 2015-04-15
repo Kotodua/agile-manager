a709de5302b0134c01d235de35521e3e3811f87a
Konstantin <kotodua@gmail.com>
2015-04-15 10:50:30 +0300

    Added Lists element to the configurations tab.

--- public/js/configurations.js
+++ public/js/configurations.js
@@ -5,21 +5,22 @@ function configurations(){
 
     mainURL = config.url+"/api/configurations";
     var verifier = new Verifier();
+    var activeElement;
 
 
     var treeConfigs = dhtmlXTreeFromHTML("treebox_configurations","100%","100%",0);
     treeConfigs.enableTreeLines(true);
-
-    var activeElement;
-
     treeConfigs.setImagePath("vendor/dhtmlx/sources/dhtmlxTree/codebase/imgs/dhxtree_terrace/");
 
+    var treeLists = dhtmlXTreeFromHTML("treebox_lists","100%","100%",0);
+    treeLists.enableTreeLines(true);
+    treeLists.setImagePath("vendor/dhtmlx/sources/dhtmlxTree/codebase/imgs/dhxtree_terrace/");
 
 
-    drawConfs(treeConfigs);
+    drawConfs(treeConfigs, treeLists);
 
 
-    function drawConfs(tree){
+    function drawConfs(treeConfigs, treeLists){
         $.ajax({
             type: "GET",
             dataType: "html",
@@ -33,22 +34,19 @@ function configurations(){
                 for (var i = 0; i < allData.conf_configurations.length; i++){
                     arrFolders.push([allData.conf_configurations[i].id,0,allData.conf_configurations[i].name]);
                 }
-                tree.loadJSArray(arrFolders);
 
+                treeConfigs.loadJSArray(arrFolders);
 
-                for (var i = 0; i < allData.conf_configurations.length; i++){
-                    $('#configurations').append('<div>'+allData.conf_configurations[i].name+'</div></br>');
-                }
 
                 for (var i = 0; i < allData.conf_list.length; i++){
-                    $('#list').append('<div>'+allData.conf_list[i].name+'</div></br>');
+                    treeLists.insertNewItem(0, allData.conf_list[i].id+'_list', allData.conf_list[i].name, 0, 0, 0, 0);
                     for (var j = 0; j < allData.conf_listitems.length; j++){
-                        if (allData.conf_list[i].id == allData.conf_listitems[j].lid)
-                        $('#list').append('<div>'+allData.conf_listitems[j].name+'</div></br>');
+                        if (allData.conf_list[i].id == allData.conf_listitems[j].lid) {
+                            treeLists.insertNewItem(allData.conf_listitems[j].lid+'_list', allData.conf_listitems[j].id, allData.conf_listitems[j].name, 0, 0, 0, 0);
+                        treeLists.insertNewItem()
+                        }
                     }
                 }
-
-
             }
         })
     }
