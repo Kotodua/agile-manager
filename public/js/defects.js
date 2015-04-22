662205f25cbf264b0dded448d3b306540b5b08cd
Konstantin <kotodua@gmail.com>
2015-04-22 10:45:55 +0300

    Implemented show defect details on select.

--- public/js/defects.js
+++ public/js/defects.js
@@ -22,18 +22,34 @@ function defects() {
 
     });
 
+    $('#new').on("click", function(){
+        $('#d-id').val("");
+        $('#d-status').val("1");
+        $('#d-severity').val("1");
+        $('#d-summary').val("");
+        $('#d-description').val("");
+        $('#d-priority').val("");
+        $('#d-reporter').val("");
+        $('#d-developer').val("");
+        $('#d-build-found').val("");
+        $('#d-build-fixed').val("");
+        $('#d-comments').val("");
+    });
 
-   /* function showDefectInfo(id){
+    function showDefectInfo(id){
+        console.log(defects2[id]);
         $('#d-id').val(id);
-        console.log(defects2);
-
-        for (var pn in defects2){
-            console.log(pn);
-            console.log(defects2.pn);
-
-        }
-        $('#d-status').val(id);
-    }*/
+        $('#d-status').val(defects2[id].sid);
+        $('#d-severity').val(defects2[id].sevid);
+        $('#d-summary').val(defects2[id].summary);
+        $('#d-description').val(defects2[id].description);
+        $('#d-priority').val(defects2[id].pid);
+        $('#d-reporter').val(user[defects2[id].rid]);
+        $('#d-developer').val(defects2[id].did);
+        $('#d-build-found').val(defects2[id].bdetected);
+        $('#d-build-fixed').val(defects2[id].bfixed);
+        $('#d-comments').val(defects2[id].comments);
+    }
 
 
     function drawDefects() {
@@ -45,28 +61,45 @@ function defects() {
                 var allData = JSON.parse(data);
                 defects = allData.defect;
 
-               /* defects2 = allData.defect.reduce(function(previousValue, currentDigit, currentIndex, array){
-                    previousValue[array[currentIndex].id+'_some'] = array[currentIndex];
+                defects2 = allData.defect.reduce(function(previousValue, currentDigit, currentIndex, array){
+                    previousValue[array[currentIndex].id] = array[currentIndex];
                     return  previousValue;
-                }, {});*/
+                }, {});
 
                 user = allData.user.reduce(function(previousValue, currentDigit, currentIndex, array){
                     previousValue[array[currentIndex].id] = array[currentIndex].pname;
+                    $('#d-developer').append($('<option>', {
+                        value: array[currentIndex].id,
+                        text: array[currentIndex].pname
+                    }));
                     return  previousValue;
                 }, {});
 
                 status = allData.defect_status.reduce(function(previousValue, currentDigit, currentIndex, array){
                     previousValue[array[currentIndex].id] = array[currentIndex].name;
+                    //$('#d-status option[value='+array[currentIndex].name+']');
+                    $('#d-status').append($('<option>', {
+                        value: array[currentIndex].id,
+                        text: array[currentIndex].name
+                    }));
                     return  previousValue;
                 }, {});
 
                 severity = allData.defect_severity.reduce(function(previousValue, currentDigit, currentIndex, array){
                     previousValue[array[currentIndex].id] = array[currentIndex].name;
+                    $('#d-severity').append($('<option>', {
+                        value: array[currentIndex].id,
+                        text: array[currentIndex].name
+                    }));
                     return  previousValue;
                 }, {});
 
                 type = allData.defect_type.reduce(function(previousValue, currentDigit, currentIndex, array){
                     previousValue[array[currentIndex].id] = array[currentIndex].name;
+                    $('#d-type').append($('<option>', {
+                        value: array[currentIndex].id,
+                        text: array[currentIndex].name
+                    }));
                     return  previousValue;
                 }, {});
 
