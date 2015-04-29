7647352fa65e628c5c797b7f9696d9cbc57eb7a2
Kostiantyn Allakhvierdov <kostiantyn.allakhvierdov@avid.com>
2015-04-29 19:41:07 +0300

    added history field (doesn't work). Changed comment's button. Implemented symbols limit. Implemented new defect and update refresh problem. selected defect highlight change. Submit\Update button frozen without changes. Internal refactoring: removed defects array. defects2 object renamed to defects.

--- public/js/defects.js
+++ public/js/defects.js
@@ -5,19 +5,28 @@
 function defects() {
     mainURL = config.url + "/api/defects";
     var verifier = new Verifier();
-    var defects, user, status, severity, type, feature, defects2, sessionUser, selectedId;
+    var defects, user, status, severity, type, feature, sessionUser, selectedId;
     drawDefects();
     filtering();
 
+    $('#d-new').on("click", function () {
+        showForm();
+        showNewDefectInfo()
+    });
+
     $("body").on("click", 'tr[id^="defect_"]', function () {
+
+        $('tr[id^="defect_"]').attr('class', 'd-regular');
         var id = $(this).prop("id");
         id = id.split('_');
         $('#submit').val("Update");
         selectedId = parseInt(id[1]);
-        $('#d-l-description').val(defects2[selectedId].description);
-        $('#d-l-summary').val(defects2[selectedId].summary);
-        $('#d-l-comments').val(defects2[selectedId].comments);
+        $('#d-l-description').val(defects[selectedId].description);
+        $('#d-l-summary').val(defects[selectedId].summary);
+        $('#d-l-comments').val(defects[selectedId].comments);
         $("#d-edit").attr('disabled', false);
+        console.log(defects[selectedId]);
+        $(this).attr('class', 'defect-selected');
     });
 
     $("body").on("dblclick", 'tr[id^="defect_"]', function () {
@@ -29,47 +38,9 @@ function defects() {
         $('.defect-dialog .ui-button-text:contains(Submit)').text('Update');
     })
 
-    $('#ewDefectForm').validate({
-        rules: {
-            d_summary: "required"
-        }
-    });
-
-
-    function getDate() {
-        var d = new Date();
-        var min = '' + d.getMinutes();
-        var hour = '' + d.getHours()
-        if (min.length < 2) min = '0' + min;
-        if (hour.length < 2) hour = '0' + hour;
-        return user[sessionUser] + ' ' + d.getFullYear() + '-' + (parseInt(d.getMonth()) + 1) + '-' + d.getUTCDate() + ' ' + hour + ':' + min + ':\n\n\n';
-    }
-
-    $('#insert-comment').on("click", function () {
-        var comment = $('#d-comments').val();
-        $('#d-comments').val(getDate() + comment);
-    })
 
 
     $('#newDefect').hide();
-
-    $('#clear').on("click", function () {
-        $('#d_id').val("");
-        $('#d-status').val("1");
-        $('#d-severity').val("1");
-        $('#d_summary').val("");
-        $('#d-l-summary').val("");
-        $('#d-description').val("");
-        $('#d-l-description').val("");
-        $('#d-priority').val("");
-        $('#d-developer').val("");
-        $('#d-build-found').val("");
-        $('#d-build-fixed').val("");
-        $('#d-comments').val("");
-        $('#d-l-comments').val("");
-        $('#submit').val("Submit");
-        newDefect();
-    });
 
     $('#d-edit').on("click", function () {
         showForm();
@@ -77,169 +48,182 @@ function defects() {
         $('.defect-dialog .ui-button-text:contains(Submit)').text('Update');
     });
 
-
-    function filtering() {
-        //------------------------filtering
-        $('.filterable .filters input').keyup(function (e) {
-            /* Ignore tab key */
-            var code = e.keyCode || e.which;
-            if (code == '9') return;
-            /* Useful DOM data and selectors */
-            var $input = $(this),
-                inputContent = $input.val().toLowerCase(),
-                $panel = $input.parents('.filterable'),
-                column = $panel.find('.filters th').index($input.parents('th')),
-                $table = $panel.find('.table'),
-                $rows = $table.find('tbody tr');
-            /* Dirtiest filter function ever ;) */
-            var $filteredRows = $rows.filter(function () {
-                var value = $(this).find('td').eq(column).text().toLowerCase();
-                return value.indexOf(inputContent) === -1;
-            });
-            /* Clean previous no-result if exist */
-            $table.find('tbody .no-result').remove();
-            /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
-            $rows.show();
-            $filteredRows.hide();
-            /* Prepend no-result row if all rows are filtered */
-            if ($filteredRows.length === $rows.length) {
-                $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="' + $table.find('.filters th').length + '">No result found</td></tr>'));
-            }
-        });
-
-    }
-
-
-
-    function showDefectInfo(id) {
-
-        $('#d_id').val(id);
-        $('#d_summary').val(defects2[id].summary);
-        $('#d-description').val(defects2[id].description);
-        $('#d-reporter').val(user[defects2[id].rid]);
-        $('#d-comments').val(defects2[id].comments);
-        $('#d-build-found').val(defects2[id].bdetected);
-        $('#d-build-fixed').val(defects2[id].bfixed);
-
-        $('#d-status').val(defects2[id].sid);
+    function refeshDefectFields(){
         $('#d-status').selectmenu('refresh', true);
-        $('#d-severity').val(defects2[id].sevid);
         $('#d-severity').selectmenu('refresh', true);
-        $('#d-priority').val(defects2[id].pid);
         $('#d-priority').selectmenu('refresh', true);
-        $('#d-type').val(defects2[id].tid);
         $('#d-type').selectmenu('refresh', true);
-        $('#d-feature').val(defects2[id].fid);
         $('#d-feature').selectmenu('refresh', true);
-        $('#d-developer').val(defects2[id].did);
         $('#d-developer').selectmenu('refresh', true);
 
-
-
     }
 
+    function showDefectInfo(id) {
+        $('#d_id').val(id);
+        $('#d_summary').val(defects[id].summary);
+        $('#d-description').val(defects[id].description);
+        $('#d-reporter').val(user[defects[id].rid]);
+        $('#d-comments').val(defects[id].comments);
+        $('#d-build-found').val(defects[id].bdetected);
+        $('#d-build-fixed').val(defects[id].bfixed);
+        $('#d-status').val(defects[id].sid);
+        $('#d-severity').val(defects[id].sevid);
+        $('#d-priority').val(defects[id].pid);
+        $('#d-type').val(defects[id].tid);
+        $('#d-feature').val(defects[id].fid);
+        $('#d-developer').val(defects[id].did);
+        refeshDefectFields();
+    }
+
+    function showNewDefectInfo(){
+        $('#d-reporter').val(user[sessionUser]);
+        $('#d_id').val("");
+        $('#d-status').val("1");
+        $('#d-severity').val("1");
+        $('#d_summary').val("");
+        $('#d-l-summary').val("");
+        $('#d-description').val("");
+        $('#d-l-description').val("");
+        $('#d-priority').val("1");
+        $('#d-developer').val(sessionUser);
+        $('#d-build-found').val("");
+        $('#d-build-fixed').val("");
+        $('#d-comments').val("");
+        $('#d-l-comments').val("");
+        $('#submit').val("Submit");
+        refeshDefectFields()
+    }
 
     function postDefect() {
-        var data;
-        //console.log(sid);
+        var data = {
+            sid: $('#d-status').val(),
+            summary: $('#d_summary').val(),
+            description: $('#d-description').val(),
+            pid: $('#d-priority').val(),
+            rid: $('#d-reporter').val(),
+            did: $('#d-developer').val(),
+            bdetected: $('#d-build-found').val(),
+            bfixed: $('#d-build-fixed').val(),
+            sevid: $('#d-severity').val(),
+            comments: $('#d-comments').val(),
+            fid: $('#d-feature').val(),
+            tid: $('#d-type').val()
+        }
         $.ajax({
             type: "POST",
-            data: {
-                sid: $('#d-status').val(),
-                summary: $('#d_summary').val(),
-                description: $('#d-description').val(),
-                pid: $('#d-priority').val(),
-                rid: $('#d-reporter').val(),
-                did: $('#d-developer').val(),
-                bdetected: $('#d-build-found').val(),
-                bfixed: $('#d-build-fixed').val(),
-                sevid: $('#d-severity').val(),
-                comments: $('#d-comments').val(),
-                fid: $('#d-feature').val(),
-                tid: $('#d-type').val()
-            },
+            data: data,
             dataType: "json",
             url: mainURL + '/postDefect',
             success: function (res) {
-                console.log(res.insertId);
+                data.id = res.insertId;
+                data.rid = sessionUser;
+                var key = res.insertId;
+                defects[key] = data;
+                drawDefect(data);
             }
         })
     }
 
     function updateDefect(id) {
+        var data = {
+            sid: $('#d-status').val(),
+            summary: $('#d_summary').val(),
+            description: $('#d-description').val(),
+            pid: $('#d-priority').val(),
+            did: $('#d-developer').val(),
+            bdetected: $('#d-build-found').val(),
+            bfixed: $('#d-build-fixed').val(),
+            sevid: $('#d-severity').val(),
+            comments: $('#d-comments').val(),
+            fid: $('#d-feature').val(),
+            tid: $('#d-type').val()
+        }
         $.ajax({
             type: "POST",
-            data: {
-                sid: $('#d-status').val(),
-                summary: $('#d_summary').val(),
-                description: $('#d-description').val(),
-                pid: $('#d-priority').val(),
-                did: $('#d-developer').val(),
-                bdetected: $('#d-build-found').val(),
-                bfixed: $('#d-build-fixed').val(),
-                sevid: $('#d-severity').val(),
-                comments: $('#d-comments').val(),
-                fid: $('#d-feature').val(),
-                tid: $('#d-type').val()
-            },
+            data: data,
             dataType: "json",
             url: mainURL + '/postDefect/' + id,
             success: function (res) {
-                console.log(res);
             }
         })
+        data.id = id;
+        data.rid = sessionUser;
+        defects[id] = data;
+        $('#defect_'+id).remove();
+        drawDefect(data);
     }
 
 
-    function drawDefect(i) {
-        var html = '<tr id=defect_' + defects[i].id + '>' +
-            '<td id="td_id">' + defects[i].id + '</td>' +
-            '<td id="td_summary">' + defects[i].summary + '</td>' +
-            '<td id="td_status">' + status[defects[i].sid] + '</td>' +
-            '<td id="td_sev">' + severity[defects[i].sevid] + '</td>' +
-            '<td id="td_p">' + defects[i].pid + '</td>' +
-            '<td id="td_r">' + user[defects[i].rid] + '</td>' +
-            '<td id="td_d">' + user[defects[i].did] + '</td>' +
-            '<td id="td_db">' + defects[i].bdetected + '</td>' +
-            '<td id="td_bf">' + defects[i].bfixed + '</td>' +
-            '<td id="td_t">' + type[defects[i].tid] + '</td>' +
-            '<td id="td_f">' + feature[defects[i].fid] + '</td>' +
+    function drawDefect(defect) {
+        var html = '<tr class="d-regular" id=defect_' + defect.id + '>' +
+            '<td id="td_id">' + defect.id + '</td>' +
+            '<td id="td_summary">' + defect.summary + '</td>' +
+            '<td id="td_status">' + status[defect.sid] + '</td>' +
+            '<td id="td_sev">' + severity[defect.sevid] + '</td>' +
+            '<td id="td_p">' + defect.pid + '</td>' +
+            '<td id="td_r">' + user[defect.rid] + '</td>' +
+            '<td id="td_d">' + user[defect.did] + '</td>' +
+            '<td id="td_db">' + defect.bdetected + '</td>' +
+            '<td id="td_bf">' + defect.bfixed + '</td>' +
+            '<td id="td_t">' + type[defect.tid] + '</td>' +
+            '<td id="td_f">' + feature[defect.fid] + '</td>' +
             '</tr>'
         $('#d-list').append(html);
         $('#d-reporter').val(user[sessionUser]);
-
     }
 
-
-    function newDefect() {
-        $('#d-reporter').val(user[sessionUser]);
-        showForm();
-
-/*        $('.defect-dialog .ui-button-text:contains(Submit)').attr("disabled", true)
-            .addClass("ui-state-disabled");
-        $('.defect-dialog .ui-button-text:contains(Cancel)').button("disable");*/
-    }
 
     function showForm() {
         $('#newDefect').dialog({
             modal: true,
             dialogClass: 'defect-dialog',
             buttons: {
-                Cancel: function () {
-                    $(this).dialog("close");
+                Insert:{
+                    click: function(){
+                        console.log('called insert comment');
+                        var comment = $('#d-comments').val();
+                        $('#d-comments').val(getDate() + comment);
+                    },
+                    text: "Insert comment"
                 },
-                Submit: function () {
-                    if ($('#d_id').val() == '') {
-                        postDefect();
-                    } else {
+                Cancel: {
+                    click: function () {
+                    $(this).dialog("close");
+                    },
+                    id: "btn_cancel",
+                    text: "Cancel"
+                },
+                Submit: {
+                    click: function () {
+                       $('#d-notification').remove();
+                       if ($('#d_id').val() == '') {
+                            postDefect();
+                           $(".ui-dialog-buttonpane").append("<div id='d-notification'>Defect successfuly Submitted.</div>");
+                           showNewDefectInfo()
+                           $('#btn_submit').attr("disabled", true).addClass("ui-state-disabled");
+                       } else {
                         updateDefect($('#d_id').val());
-                    }
+                        $(".ui-dialog-buttonpane").append("<div id='d-notification'>Defect successfuly Updated.</div>");
+                        $('#btn_submit').attr("disabled", true).addClass("ui-state-disabled");
+                       }
+                    },
+                    id: "btn_submit",
+                    text: "Submit"
                 }
             },
-            width: "25%"
+            width: "27%",
+            heigth: "40%"
         })
 
-        $(".ui-button-text button:contains('Submit')").button("disable");
+        maxLength();
+        $('#btn_submit').attr("disabled", true).addClass("ui-state-disabled");
+        $('#newDefect').children().each(function (i, v) {
+            $(v).on('change', function(){
+                console.log(v + ' changed.')
+                $('#btn_submit').attr("disabled", false).removeClass("ui-state-disabled");
+                $('#d-notification').remove();
+            });
+        });
 
         $("#d-status").selectmenu();
         $("#d-severity").selectmenu();
@@ -247,7 +231,6 @@ function defects() {
         $("#d-developer").selectmenu();
         $("#d-type").selectmenu();
         $("#d-feature").selectmenu();
-
 
         $("#newDefect").show('bounce', {}, 500, function callback() {
             setTimeout(function () {
@@ -285,11 +268,8 @@ function defects() {
             url: mainURL + '/getDefects',
             success: function (data) {
                 var allData = JSON.parse(data);
-                defects = allData.defect;
                 sessionUser = allData.currentUser;
-
-
-                defects2 = allData.defect.reduce(function(previousValue, currentDigit, currentIndex, array){
+                defects = allData.defect.reduce(function(previousValue, currentDigit, currentIndex, array){
                     previousValue[array[currentIndex].id] = array[currentIndex];
                     return  previousValue;
                 }, {});
@@ -328,27 +308,86 @@ function defects() {
                             text: arr[ci].name
                         }))
                     }
-
-
                     return  pv;
                 }
 
-                for (var i = 0; i < defects.length; i++){
-                     drawDefect(i);
-
-
-
-
-
+                for( var key in defects){
+                    if(defects.hasOwnProperty(key)){
+                        drawDefect(defects[key]);
+                    }
                 }
 
                 resizeTable()
-
-
-                $( "#tabs" ).tabs();
-
-
             }
         })
     }
+
+    function maxLength(){
+        var txts = document.getElementsByTagName('textarea');
+        for(var i = 0, l = txts.length; i < l; i++) {
+            if(txts[i].getAttribute("maxlength")) {
+                var func = function() {
+                    var len = parseInt(this.getAttribute("maxlength"), 10);
+                    if(this.value.length > len-1) {
+                        $('#d-notification').remove();
+                        $(".ui-dialog-buttonpane").append("<div id='d-notification'>Max length achieved</div>");
+                        this.value = this.value.substr(0, len);
+                        return false;
+                    } else{
+                        $('#d-notification').remove();
+                        $(".ui-dialog-buttonpane").append("<div id='d-notification'>Left "+(len-this.value.length)+" symbols</div>");
+                    }
+                }
+                txts[i].onkeyup = func;
+                txts[i].onfocus = func;
+            } else {
+                   $('#d-notification').remove();
+
+            }
+        }
+
+    }
+
+
+
+
+    function filtering() {
+        //------------------------filtering
+        $('.filterable .filters input').keyup(function (e) {
+            /* Ignore tab key */
+            var code = e.keyCode || e.which;
+            if (code == '9') return;
+            /* Useful DOM data and selectors */
+            var $input = $(this),
+                inputContent = $input.val().toLowerCase(),
+                $panel = $input.parents('.filterable'),
+                column = $panel.find('.filters th').index($input.parents('th')),
+                $table = $panel.find('.table'),
+                $rows = $table.find('tbody tr');
+            /* Dirtiest filter function ever ;) */
+            var $filteredRows = $rows.filter(function () {
+                var value = $(this).find('td').eq(column).text().toLowerCase();
+                return value.indexOf(inputContent) === -1;
+            });
+            /* Clean previous no-result if exist */
+            $table.find('tbody .no-result').remove();
+            /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
+            $rows.show();
+            $filteredRows.hide();
+            /* Prepend no-result row if all rows are filtered */
+            if ($filteredRows.length === $rows.length) {
+                $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="' + $table.find('.filters th').length + '">No result found</td></tr>'));
+            }
+        });
+    }
+
+    function getDate() {
+        var d = new Date();
+        var min = '' + d.getMinutes();
+        var hour = '' + d.getHours()
+        if (min.length < 2) min = '0' + min;
+        if (hour.length < 2) hour = '0' + hour;
+        return user[sessionUser] + ' ' + d.getFullYear() + '-' + (parseInt(d.getMonth()) + 1) + '-' + d.getUTCDate() + ' ' + hour + ':' + min + ':\n\n\n';
+    }
 }
+
